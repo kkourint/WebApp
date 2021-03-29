caae8cd40a458e57435e5ad7854d18d3b24489da
kkourint1 <bepositive1900@hotmail.com>
2021-03-28 22:44:33 -0500

    MVC

--- lab08/index.php
+++ lab08/index.php
@@ -47,6 +47,34 @@ if ($action==='list-games'){
     $pageTitle= 'Update Game';
     include 'view/games_update.php';
 }
+elseif ($action ==='update-game'){
+    $id = filter_input(INPUT_POST, 'ID', FILTER_SANITIZE_NUMBER_INT);
+    $Opponent = filter_input(INPUT_POST, 'opponent', FILTER_SANITIZE_STRING);
+    $Goals = filter_input(INPUT_POST, 'number-goals', FILTER_SANITIZE_NUMBER_INT);
+    $Spectators = filter_input(INPUT_POST, 'number-spectators', FILTER_SANITIZE_NUMBER_INT);
+    $Location = filter_input(INPUT_POST, 'game-location', FILTER_SANITIZE_STRING);
+    $GameType = filter_input(INPUT_POST, 'game-type');
+
+    if (!strlen($Opponent) || !strlen($Goals) || !strlen($Spectators) || !strlen($Location)) {
+        $error = 'All fields in the Update form must contain data. Please ensure all form element contain appropriate values.';
+        $game=getGameInfo($id);
+        $pageTitle = 'Update Game';
+        include 'view/games_update.php';
+    } else {
+        updateGame($id, $Opponent, $Goals, $Spectators, $Location, $GameType);
+        $games = getAllGames();
+        $pageTitle ='List Movies';
+        include 'view/games_list.php';
+    }
+
+}elseif ($action === 'delete-movie'){
+    $id = filter_input(INPUT_POST, 'ID', FILTER_SANITIZE_NUMBER_INT);
+    $game =getGameInfo($id);
+    $Opponent = $game['Opponent'];
+    deleteGame($id, $Opponent);
+    $games = getAllGames();
+    $pageTitle= 'List Games';
+}
 
 elseif($action === 'clear=message'){
   header('Location:.');
--- lab08/model/games_db.php
+++ lab08/model/games_db.php
@@ -54,3 +54,47 @@ function getGameInfo($id){
         }
         return $game;
 }
+function updateGame($id, $Opponent, $Goals, $Spectators, $Location, $GameType){
+    global $db;
+    global $error, $successMessage;
+    $query = 'UPDATE games
+                SET ID= :Game_id,
+                Opponent= :Opponent,
+                Goals= :Goals,
+                Spectators=:Spectators,
+                Location= :Location,
+                GameType=:GameType
+             WHERE ID = :Game_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':Game_id', $id, PDO::PARAM_INT);
+    $statement->bindValue(':$Opponent', $Opponent);
+    $statement->bindValue(':$Goals', $Goals);
+    $statement->bindValue(':$Spectators', $Spectators);
+    $statement->bindValue(':$Location', $Location);
+    $statement->bindValue(':$GameType', $GameType);
+    $statement = $statement->closeCursor();
+    $statement->closeCursor();
+    if ($statement->errorCode() !==0 && $success ===false){
+        $sqlError->$statement->errorInfor();
+        $error = 'UPDATE error &rarr; The game <strong>' . '</strong> was not updated because: ' . $sqlError[2];
+    }else{
+        $successMessage = 'The game <strong>' . $Opponent . '</strong> was successfully updated.';
+    }
+}
+function deleteGame($id, $Opponent){
+    global $db;
+    global $error, $successMessage;
+    $query = 'DELETE FROM games WHERE ID = :Game_id';
+    $statement = $db->prepare($query);
+    $statement ->bindValue(':Game_id', $id, POD::PARAM_INT);
+    $success = $statement->execute();
+    $statement->closeCursor();
+    if ($statement->errorCode() !== 0 && $success === false) {
+        $sqlError = $statement->errorInfo();
+        $error = 'DELETE error &rarr; The game <strong>' . $Opponent . '</strong> was successfully deleted.';
+
+    }else{
+        $successMessage = 'The game <strong>' . $Opponent . '</strong> was successfully deleted';
+
+    }
+}
